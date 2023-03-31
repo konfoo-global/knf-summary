e628df9f732b6b4c82fb1fea594501a5a225d2bf
n0n <martti@avalah.ee>
2023-03-31 15:16:09 +0300

    Add: simple summary component

--- src/KnfSummary.js
+++ src/KnfSummary.js
@@ -13,8 +13,10 @@ export class KnfSummary extends LitElement {
 		header: { type: String },
 		currency: { type: String },
 		aggregator: { type: String },
+		field: { type: String },
 		api: { type: String },
 		session: { type: String },
+		total: { type: String },
 	};
 
 	constructor() {
@@ -25,28 +27,74 @@ export class KnfSummary extends LitElement {
 		this.api = null;
 		this.session = null;
 		this.total = 0;
+
+		this._eventListener = undefined;
 	}
 
 	connectedCallback() {
 		super.connectedCallback();
-		window.addEventListener('konfoo-message', this.onMessage);
 
-		// console.log('window = ', window);
-		// window.dispatchEvent(new CustomEvent('konfoo-message', { detail: 'wtf' }));
+		const self = this;
+		this._eventListener = function onMessageWrapper(event) {
+			self.onMessage(event);
+		};
+		window.addEventListener('konfoo-message', this._eventListener);
+
+		this.updateSummary();
 	}
 
 	disconnectedCallback() {
 		super.disconnectedCallback();
-		window.removeEventListener('konfoo-message', this.onMessage);
+		if (this._eventListener) {
+			window.removeEventListener('konfoo-message', this._eventListener);
+		}
 	}
 
 	onMessage(event) {
-		console.log('[component] konfoo-message', event.detail);
+		if (
+			typeof event.detail !== 'object' ||
+			event.detail.type !== 'konfoo' ||
+			typeof event.detail.cmd !== 'string'
+		) {
+			return;
+		}
+		const msg = event.detail;
+
+		if (msg.cmd === 'change') {
+			this.updateSummary();
+		}
 	}
 
 	render() {
 		return html`
 			<h2>${this.header}: ${this.total}${this.currency || ''}</h2>
 		`;
+	}
+
+	updateSummary() {
+		let total = 0;
+		fetch(`${this.api}/agg/${this.aggregator}/${this.session}`)
+			.then(response => response.json())
+			.then(data => {
+				if (!data.data) {
+					return;
+				}
+				for (const line of data.data) {
+					if (!line || !line[this.field]) {
+						continue;
+					}
+
+					const value = parseFloat(line[this.field]);
+					if (isNaN(value)) continue;
+
+					total += value;
+				}
+			})
+			.then(() => {
+				this.total = total;
+			})
+			.catch(err => {
+				console.error('Could not fetch aggregator data:', err);
+			});
 	}
 }
